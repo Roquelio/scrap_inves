d8bafc3afd162daf2cc78e06c92685c4146cca9a
Roquelio <androroques03@gmail.com>
2024-02-17 15:12:20 -0500

    test_scrap_inves_1.2

--- index.py
+++ index.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
+import os
 
 def crear_app():
     app = Flask(__name__)
+
     @app.route('/')
     def home():
         return render_template('html.html')
+
     return app
 
+app = crear_app()
+
 if __name__ == '__main__':
-    app = crear_app()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
